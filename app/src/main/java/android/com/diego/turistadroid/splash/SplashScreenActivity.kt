8f068d88c0d176a84e2397b38ba1c2fbfb730bdb
Diego Sanchez <diegozj92@gmail.com>
2020-11-26 12:15:44 +0100

    Creada SplashActivity

---
+++ app/src/main/java/android/com/diego/turistadroid/splash/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package android.com.diego.turistadroid.splash
+
+import android.com.diego.turistadroid.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
