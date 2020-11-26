71538581ce74d4fbbda52f99c15a0ffc30e59698
GuillermoArredondo <guillerapc@gmail.com>
2020-11-26 12:14:41 +0100

    Commit 1: Creada SignUp (activity)

---
+++ app/src/main/java/android/com/diego/turistadroid/signup/SignUp.kt
@@ -0,0 +1,12 @@
+package android.com.diego.turistadroid.signup
+
+import android.com.diego.turistadroid.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
