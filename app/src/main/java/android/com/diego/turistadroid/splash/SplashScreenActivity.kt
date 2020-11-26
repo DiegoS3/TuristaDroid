072fbd5d320a97cd69fc0879ca09f3e4ede4f382
Diego Sanchez <diegozj92@gmail.com>
2020-11-26 14:34:34 +0100

    Retoques Animaciones

--- app/src/main/java/android/com/diego/turistadroid/splash/SplashScreenActivity.kt
+++ app/src/main/java/android/com/diego/turistadroid/splash/SplashScreenActivity.kt
@@ -14,7 +14,7 @@ import android.widget.TextView
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val TIME : Long = 10000
+    private val TIME : Long = 5000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,29 +23,10 @@ class SplashScreenActivity : AppCompatActivity() {
         this.supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
 
-        val animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
-        val animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo)
-        val animacion3 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_lateral)
-        val animacion4 = AnimationUtils.loadAnimation(this, R.anim.fade_pines)
+        //iniciamos las animaciones
+        initAnimations()
 
-        val developText = findViewById<TextView>(R.id.textDevelopment)
-        val titleText = findViewById<TextView>(R.id.textTitleSplash)
-        val logoImg = findViewById<ImageView>(R.id.imgMundo_Splash)
-        val pinesImg = findViewById<ImageView>(R.id.imgPines_Splash)
-
-        developText.startAnimation(animacion2)
-        titleText.startAnimation(animacion1)
-        logoImg.startAnimation(animacion3)
-        pinesImg.startAnimation(animacion4)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            run {
-
-
-
-            }
-        }, 1000)
-
+        //cargamos la login activity tras pasar X tiempo
         val main = Intent(this, MainActivity::class.java)
         Handler(Looper.getMainLooper()).postDelayed({
             run {
@@ -53,7 +34,69 @@ class SplashScreenActivity : AppCompatActivity() {
                 finish()
             }
         }, this.TIME)
+    }
 
+    /**
+     * Metodo que contiene otros metodos que inician las animaciones
+     */
+    private fun initAnimations(){
+        
+        animacionLetras()
+        animacionMundo()
+        animacionesPines()
+    }
+
+    /**
+     * Animaciones correspondientes a las letras de la Splash Screen
+     */
+    private fun animacionLetras() {
+        //Animaciones que usaremos
+        val animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
+        val animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo)
+
+        //Diferentes componentes del layout
+        val developText = findViewById<TextView>(R.id.textDevelopment)
+        val titleText = findViewById<TextView>(R.id.textTitleSplash)
+
+        //Asignamos la animación correspondiente a cada uno de los componentes
+        developText.startAnimation(animacion2)
+        titleText.startAnimation(animacion1)
+    }
+
+    /**
+     * Animaciones correspondientes al mundo del logo del Splash Screen
+     */
+    private fun animacionMundo(){
+
+        //Animaciones que usaremos
+        val animacion3 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_lateral)
+
+        //Diferentes componentes del layout
+        val logoImg = findViewById<ImageView>(R.id.imgMundo_Splash)
+
+        //Asignamos la animación correspondiente a cada uno de los componentes
+        logoImg.startAnimation(animacion3)
+
+    }
+    /**
+     * Animaciones correspondientes a los pines del mundo de la Splash Screen
+     */
+    private fun animacionesPines(){
+
+        //Animaciones que usaremos
+        val animacion4 = AnimationUtils.loadAnimation(this, R.anim.fade_pines)
+        val animacion5 = AnimationUtils.loadAnimation(this, R.anim.fade_pines1)
+        val animacion6 = AnimationUtils.loadAnimation(this, R.anim.fade_pines2)
+
+        //Diferentes componentes del layout
+        val pinesImg = findViewById<ImageView>(R.id.imgPines_Splash)
+        val pinesImg1 = findViewById<ImageView>(R.id.imgPines1_Splash)
+        val pinesImg2 = findViewById<ImageView>(R.id.imgPines2_Splash)
+
+        //Asignamos la animación correspondiente a cada uno de los componentes
+        pinesImg.startAnimation(animacion4)
+        pinesImg1.startAnimation(animacion5)
+        pinesImg2.startAnimation(animacion6)
 
     }
 }
