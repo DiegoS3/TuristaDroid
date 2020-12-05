84d43e10f81b3fa1876f911f1dd3f59885412088
Diego Sanchez <diegozj92@gmail.com>
2020-12-05 22:25:01 +0100

    Fragment New Place

---
+++ app/src/main/java/android/com/diego/turistadroid/utilities/slider/SliderAdapter.kt
@@ -0,0 +1,4 @@
+package android.com.diego.turistadroid.utilities.slider
+
+class SliderAdapter {
+}
---
+++ app/src/main/java/android/com/diego/turistadroid/utilities/slider/SliderItem.kt
@@ -0,0 +1,5 @@
+package android.com.diego.turistadroid.utilities
+
+class SliderItem (image : Int) {
+
+}
