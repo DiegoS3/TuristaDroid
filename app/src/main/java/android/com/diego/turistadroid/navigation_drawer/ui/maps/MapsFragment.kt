3494e7eaa7734e78961fe4f279474b6424d9b6ea
GuillermoArredondo <guillerapc@gmail.com>
2020-12-13 19:54:51 +0100

    Fix nearMe

--- app/src/main/java/android/com/diego/turistadroid/navigation_drawer/ui/maps/MapsFragment.kt
+++ app/src/main/java/android/com/diego/turistadroid/navigation_drawer/ui/maps/MapsFragment.kt
@@ -4,12 +4,17 @@ import android.com.diego.turistadroid.R
 import android.com.diego.turistadroid.navigation_drawer.ui.newplace.NewPlaceFragment
 import android.graphics.*
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -39,11 +44,31 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickList
             return@setOnTouchListener true
         }
         initUI()
+        getCurrentLocation()
     }
 
     private fun initUI() {
         initMapa()
         saveLocation()
+    }
+
+    private fun getCurrentLocation(){
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 2000
+        locationRequest.fastestInterval = 1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        LocationServices.getFusedLocationProviderClient(activity!!)
+            .requestLocationUpdates(locationRequest, object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    val latestLocationIndex = locationResult.locations.size - 1
+                    var latitud = locationResult.locations[latestLocationIndex].latitude //LATITUD
+                    var longitud = locationResult.locations[latestLocationIndex].longitude //LONGITUD
+                    val currentLocation = LatLng(latitud, longitud)
+                    moverCamara(currentLocation)
+                }
+            }, Looper.getMainLooper())
     }
 
     /**
@@ -67,10 +92,36 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickList
             location = LatLng(latLng.latitude, latLng.longitude)
             placeMarker(location)
         }
+
+        configurarIUMapa()
     }
 
+    private fun moverCamara(latLng: LatLng) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
+
+
+    private fun configurarIUMapa() {
+        Log.i("Mapa", "Configurando IU Mapa")
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val uiSettings: UiSettings = mMap.uiSettings
+        uiSettings.isScrollGesturesEnabled = true
+        uiSettings.isTiltGesturesEnabled = true
+        uiSettings.isCompassEnabled = true
+        uiSettings.isZoomControlsEnabled = true
+        uiSettings.isMapToolbarEnabled = false
+        mMap.setMinZoomPreference(5.0f)
+    }
+
+
     private fun placeMarker(location: LatLng){
-        val markerOptions = MarkerOptions().position(location)
+        val icon = BitmapDescriptorFactory.fromBitmap(
+            BitmapFactory.decodeResource(
+                context?.resources,
+                R.drawable.pin_centrado_2
+            )
+        )
+        val markerOptions = MarkerOptions().position(location).icon(icon)
         mMap.addMarker(markerOptions)
     }
 
@@ -83,7 +134,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickList
         val newFragment: Fragment = NewPlaceFragment()
         val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
         transaction.replace(R.id.nav_host_fragment, newFragment)
-        transaction.addToBackStack(null)
+        //transaction.addToBackStack(null)
         transaction.commit()
 
     }
--- app/src/main/java/android/com/diego/turistadroid/navigation_drawer/ui/nearme/NearMeFragment.kt
+++ app/src/main/java/android/com/diego/turistadroid/navigation_drawer/ui/nearme/NearMeFragment.kt
@@ -56,7 +56,6 @@ class NearMeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
         //requireActivity().actionBar!!.title = getString(R.string.near_me)
 
         getCurrentLocation()
-        anadirLugares()
         initUI()
         
 
@@ -100,118 +99,6 @@ class NearMeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickLi
     }
 
 
-    private fun anadirLugares(){
-        ControllerPlaces.deleteAllPlaces()
-        ControllerBbdd.deleteAllPlaces()
-        val fecha = Calendar.getInstance().time
-        val img = Image(
-            1, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img2 = Image(
-            2, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img3 = Image(
-            3, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img4 = Image(
-            4, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img5 = Image(
-            5, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img6 = Image(
-            6, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img7 = Image(
-            7, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img8 = Image(
-            8, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-        val img9 = Image(
-            9, Utilities.bitmapToBase64(
-                BitmapFactory.decodeResource(
-                    context!!.resources,
-                    R.drawable.ima_default_place
-                )
-            )!!
-        )
-
-        //Ciudad Real
-        val lugar = Place(1, "Lugar 1", fecha, "ciudad", 4.3, -3.940100, 38.981782)
-        var lugar2 = Place(2, "Lugar 2", fecha, "ciudad", 3.5, -3.945000, 38.981000)
-        var lugar3 = Place(3, "Lugar 3", fecha, "ciudad", 0.5, -3.940900, 38.981500)
-
-        val lugar7 = Place(7, "Lugar 1", fecha, "ciudad", 4.3, -3.918120, 38.980935)
-        var lugar8 = Place(8, "Lugar 2", fecha, "ciudad", 3.5, -3.940031, 38.993067)
-        var lugar9 = Place(9, "Lugar 3", fecha, "ciudad", 0.5, -3.942874, 38.971091)
-
-        //Puertollano
-        val lugar4 = Place(4, "Lugar 1", fecha, "ciudad", 4.3, -4.11179038, 38.707595)
-        var lugar5 = Place(5, "Lugar 2", fecha, "ciudad", 3.5, -4.11017800, 38.702733)
-        var lugar6 = Place(6, "Lugar 3", fecha, "ciudad", 0.5, -4.08364100, 38.682322)
-
-
-        lugar.imagenes.add(img)
-        lugar2.imagenes.add(img2)
-        lugar3.imagenes.add(img3)
-        lugar4.imagenes.add(img4)
-        lugar5.imagenes.add(img5)
-        lugar6.imagenes.add(img6)
-        lugar7.imagenes.add(img7)
-        lugar8.imagenes.add(img8)
-        lugar9.imagenes.add(img9)
-
-        ControllerPlaces.insertPlace(lugar)
-        ControllerPlaces.insertPlace(lugar2)
-        ControllerPlaces.insertPlace(lugar3)
-        ControllerPlaces.insertPlace(lugar4)
-        ControllerPlaces.insertPlace(lugar5)
-        ControllerPlaces.insertPlace(lugar6)
-        ControllerPlaces.insertPlace(lugar7)
-        ControllerPlaces.insertPlace(lugar8)
-        ControllerPlaces.insertPlace(lugar9)
-    }
 
 
     private fun initUI() {
